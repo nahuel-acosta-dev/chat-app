22aa7d2608336e5ff4878c7bba8aa125c6b572f5
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-10-01 20:03:22 -0300

    chat application model completed

--- backend/apps/chat/models.py
+++ backend/apps/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from apps.user_profile.models import UserProfile
 # Create your models here.
+
+
+class ChatModel(models.Model):
+    profile1 = models.ForeignKey(
+        UserProfile, models.CASCADE, blank=False, related_name="chat_user1")
+    profile2 = models.ForeignKey(
+        UserProfile, models.CASCADE, blank=False, related_name="chat_user2")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def chat_name(self):
+        if self.profile1.user_id > self.profile2.user_id:
+            return f"chat_{self.profile1.user_id}_{self.profile2.user_id}"
+        else:
+            return f"chat_{self.profile2.user_id}_{self.profile1.user_id}"
+
+    def __str__(self):
+        return f"chat of {self.profile1.user_id} and {self.profile2.user_id}"
