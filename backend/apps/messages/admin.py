d2f2736e9d18e2eea6fbbe136462a7423e2b605c
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-10-01 21:04:45 -0300

    deleted application messages

--- backend/apps/messages/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Message
-from .models import Messages
-# Register your models here.
-admin.site.register(Message)
-admin.site.register(Messages)
--- backend/apps/messages/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.messages'
-    label = 'any_unique_name'
