92a2e08f272c9b510ce12e82f66dec4992313b3e
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-11-24 20:20:29 -0300

    ResendActivationEmailScreen page design finished

--- frontend/src/routes/AuthRouter.tsx
+++ frontend/src/routes/AuthRouter.tsx
@@ -2,7 +2,7 @@ import {Routes, Route} from 'react-router-dom';
 import LoginScreen from '../pages/auth/LoginScreen';
 import RegisterScreen from '../pages/auth/RegisterScreen';
 import ActivateScreen from '../pages/auth/ActivateScreen';
-import ResendActivationEmail from '../pages/auth/ResendActivationEmail';
+import ResendActivationEmailScreen from '../pages/auth/ResendActivationEmailScreen';
 import ResetPasswordConfirmScreen from '../pages/auth/ResetPasswordConfirmScreen';
 import ResetPasswordScreen from '../pages/auth/ResetPasswordScreen';
 import GoogleButtonLogin from '../components/auth/GoogleButtonLogin';
@@ -14,7 +14,7 @@ const AuthRouter = () => {
                 <Route path='register' element={<RegisterScreen/>} />
                 <Route path='activate/:uid/:token' element={<ActivateScreen/>} />
                 <Route path='password/reset/confirm/:uid/:token' element={<ResetPasswordConfirmScreen />} />
-                <Route path='redirect/activation/email' element={<ResendActivationEmail />} />
+                <Route path='redirect/activation/email' element={<ResendActivationEmailScreen />} />
                 <Route path='reset-password' element={<ResetPasswordScreen />} />
                 <Route path='google' element={<GoogleButtonLogin/>} />
             </Routes>)
