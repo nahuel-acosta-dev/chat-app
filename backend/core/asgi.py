befd1b55ed2975bb3093cac49f54da398178496d
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-12-20 20:27:05 -0300

    change asgi

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -6,9 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-import apps.chat.routing
-import os
+
 from django.core.asgi import get_asgi_application
+import os
+import apps.chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
