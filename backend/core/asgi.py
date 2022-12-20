ed05545d65ac204ce8c6aa4355d60fd625088bed
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-12-20 19:47:11 -0300

    change asgi

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -6,7 +6,34 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
+import apps.chat.routing
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                apps.chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
+
+
+"""
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -22,4 +49,4 @@ application = ProtocolTypeRouter({
             apps.chat.routing.websocket_urlpatterns
         )
     )
-})
+})"""
