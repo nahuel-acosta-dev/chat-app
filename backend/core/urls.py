99a57f4f2d246058dafe61743573f766321079f3
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-12-22 21:31:39 -0300

    change build.sh

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -46,7 +46,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="brianacostanahuelmails@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
