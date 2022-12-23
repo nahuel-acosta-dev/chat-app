80783ad749dd2886735089ced89ed253e6e80999
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-12-22 21:14:13 -0300

    change build.sh

--- backend/render/build.sh
+++ backend/render/build.sh
@@ -10,6 +10,14 @@ python manage.py makemigrations user
 
 python manage.py makemigrations user_profile
 
+python manage.py makemigrations chat
+
+python manage.py makemigrations chat_group
+
+python manage.py makemigrations message_in_chat_group
+
+python manage.py makemigrations user_in_chat_group
+
 python manage.py makemigrations
 
 python manage.py migrate
